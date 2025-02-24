acf263783ac5f2d2435758d799f76ab31169cdde
August Heiervang Dahl <augusthdahl@gmail.com>
2025-02-24 12:03:38 +0100

    Legg til sql_endpoint_id i modulen for dask-infra (#126)
    Endring gjort under utvikling av kostnadsdashboards som ikke har blitt
    implementert i scriptet for `dask-infrastructure`

--- scripts/dask_infrastructure.py
+++ scripts/dask_infrastructure.py
@@ -40,6 +40,8 @@ def generate_module_definition(project_name: str, project_id_map: dict, env: str
       project_id              = "{project_id_map[env]}"
       common_users_and_groups = local.common_users_and_groups
       config                  = local.config
+      sql_endpoint_id         = databricks_sql_endpoint.this.id
+      env                     = var.env 
     
       providers = {{
         databricks.accounts   = databricks.accounts
