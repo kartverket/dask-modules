3a457885a983dbfb447d94a49cb2b1b1f92fdc0c
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2025-02-27 08:03:06 +0100

    Revert endringer og fjern kommentarer

--- terraform/modules/db2open/variables.tf
+++ terraform/modules/db2open/variables.tf
@@ -1,17 +1,17 @@
-# variable "recipient" {
-#   description = "List of valid recipient organization numbers."
-# }
+variable "recipient" {
+  description = "List of valid recipient organization numbers."
+}
 
-# variable "schema_name_ext" {
-#   type        = string
-#   description = "The schema name to share."
-# }
+variable "schema_name_ext" {
+  type        = string
+  description = "The schema name to share."
+}
 
-# variable "tables_to_share" {
-#   type        = list(string)
-#   default     = []
-#   description = "List of tables to share"
-# }
+variable "tables_to_share" {
+  type        = list(string)
+  default     = []
+  description = "List of tables to share"
+}
 
 variable "share_name" {
   description = "Name of share"
