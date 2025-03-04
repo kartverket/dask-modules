2294d33e4aa361b8cef66744ea916b1fe06784d0
augustdahl <augusthdahl@gmail.com>
2025-03-04 12:42:19 +0100

    Bytt fra serviceaccount til policy_data for direkte aksess til bøtta

--- terraform/modules/skyporten_workload_identity/skyporten_consumers/main.tf
+++ terraform/modules/skyporten_workload_identity/skyporten_consumers/main.tf
@@ -8,24 +8,21 @@ resource "random_string" "random" {
   upper   = false
 }
 
-resource "google_service_account" "skyporten_consumer" {
-  account_id  = "sp-${var.org_number}-${random_string.random.result}"
-  description = "Service account for the Skyporten consumer for organization ${var.org_number} with scope ${local.maskinporten_scope}"
-}
-
 data "google_iam_policy" "client_access" {
   binding {
-    role = "roles/iam.workloadIdentityUser"
+    role = "roles/storage.legacyBucketReader"
 
     members = [
       "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.clientaccess/client::${var.maskinporten_client_id}::${local.maskinporten_scope}",
     ]
   }
-}
+  binding {
+    role = "roles/storage.objectViewer"
 
-resource "google_service_account_iam_policy" "workload_identity_policy" {
-  service_account_id = google_service_account.skyporten_consumer.name
-  policy_data        = data.google_iam_policy.client_access.policy_data
+    members = [
+      "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.clientaccess/client::${var.maskinporten_client_id}::${local.maskinporten_scope}",
+    ]
+  }
 }
 
 resource "google_storage_bucket" "skyporten_bucket" {
@@ -34,15 +31,7 @@ resource "google_storage_bucket" "skyporten_bucket" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_iam_member" "legacy_bucket_reader" {
-  bucket = google_storage_bucket.skyporten_bucket.name
-  role   = "roles/storage.legacyBucketReader"
-  member = "serviceAccount:${google_service_account.skyporten_consumer.email}"
+resource "google_storage_bucket_iam_policy" "legacy_bucket_reader" {
+  bucket      = google_storage_bucket.skyporten_bucket.name
+  policy_data = data.google_iam_policy.client_access.policy_data
 }
-
-resource "google_storage_bucket_iam_member" "object_viewer" {
-  bucket = google_storage_bucket.skyporten_bucket.name
-  role   = "roles/storage.objectViewer"
-  member = "serviceAccount:${google_service_account.skyporten_consumer.email}"
-}
-
