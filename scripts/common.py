b1fd9bf098582901f562db125a3da0ea5c269ca7
Ole Kristian Pedersen <ole.pedersen@kartverket.no>
2025-03-06 09:03:22 +0100

    gcp-service-accounts: Do not inject if content already exists
    This will cause the formatting step of the workflow to fail.

--- scripts/common.py
+++ scripts/common.py
@@ -1,4 +1,5 @@
-import re 
+import re
+from typing import List
 
 def replace_special_characters(s):
     replacements = {
@@ -9,22 +10,27 @@ def replace_special_characters(s):
         'å': 'aa',
         'Å': 'Aa'
     }
-    
+
     def replace(match):
         return replacements[match.group(0)]
-    
+
     return re.sub(r'æ|Æ|ø|Ø|å|Å', replace, s)
 
+def is_content_in_file(file_path: str, content: str) -> bool:
+    with open(file_path) as file:
+        file_content = file.read()
+        return file_content.find(content) > -1
+
+
 def append_content_to_end_of_file(file_path: str, content: str) -> None:
+    lines: List[str] = []
     with open(file_path) as file:
         lines = file.readlines()
-        file.close()
-        
-        if lines and not lines[-1].endswith('\n'):
-            lines[-1] += '\n'
 
-        lines.append(content + '\n')
+    if lines and not lines[-1].endswith('\n'):
+        lines[-1] += '\n'
 
-        with open(file_path, 'w') as file:
-            file.writelines(lines)
-            file.close()
+    lines.append(content + '\n')
+
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
