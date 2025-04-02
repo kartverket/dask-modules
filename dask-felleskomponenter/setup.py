dfe123a2a1411e7953926c5043cbea69bf4f8726
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2025-04-02 08:51:51 +0200

    Oppdatere versjon til 0.0.32 som samsvarer med pypi

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.0.31",
+    version="0.0.32",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
