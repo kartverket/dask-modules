78676c9da52ba16b81822f9dd10d9cbb5ed71773
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2025-05-06 09:07:13 +0200

    Bytte til å bruke id for kodelistene. Må gjøre i markedsplassen også

--- dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/common.py
+++ dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/common.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional
-import os
-import json
+from .sikkerhetsnivaa_kodeliste import sikkerhetsnivaa_kodeliste
+from .tilgangsnivaa_kodeliste import tilgangsnivaa_kodeliste
 
 import requests
 
+medaljongnivaa_kodeliste = ["bronze", "silver", "gold"]
+
+class CodelistUrls:
+    hovedkategori = "https://register.geonorge.no/metadata-kodelister/tematisk-hovedkategori"
+    begrep = "https://register.geonorge.no/metadata-kodelister/nasjonal-temainndeling"
 
 @dataclass(init=False)
 class TableMetadata:
@@ -45,13 +50,13 @@ class MetadataError:
     valid_values: str | List[str]
 
 def get_valid_codelist_values(kodeliste_url: str, override_kodeliste_keyword: Optional[str] = None) -> List[str]:
-    kodeliste_entry = "label" if override_kodeliste_keyword == None else override_kodeliste_keyword
+    kodeliste_entry = "id" if override_kodeliste_keyword == None else override_kodeliste_keyword
     values_res = requests.get(kodeliste_url, headers={ "Accept": "application/json" }).json()
     valid_values = list(filter(lambda x: x != None, [x.get(kodeliste_entry, None) for x in values_res["containeditems"]]))
     return valid_values
 
 def get_valid_codelist_values_local(kodeliste: dict, override_kodeliste_keyword: Optional[str] = None) -> List[str]:
-    kodeliste_entry = "label" if override_kodeliste_keyword is None else override_kodeliste_keyword
+    kodeliste_entry = "id" if override_kodeliste_keyword is None else override_kodeliste_keyword
     
     valid_values = list(filter(lambda x: x is not None, 
                              [x.get(kodeliste_entry, None) for x in kodeliste["containeditems"]]))
@@ -86,6 +91,20 @@ def check_codelist_value_local(kodeliste_path: str, value: Any, allowed_values: 
 
     valid_values = get_valid_codelist_values_local(kodeliste_path, override_kodeliste_keyword)
     return value in valid_values
+
+def get_codelist(key: str) -> List[str]:
+    if key == "hovedkategori":
+        return get_valid_codelist_values(CodelistUrls.hovedkategori)
+    elif key == "begrep":
+        return get_valid_codelist_values(CodelistUrls.begrep)
+    elif key == "tilgangsnivaa":
+        return get_valid_codelist_values_local(tilgangsnivaa_kodeliste)
+    elif key == "medaljongnivaa":
+        return medaljongnivaa_kodeliste
+    elif key == "sikkerhetsnivaa":
+        return get_valid_codelist_values_local(sikkerhetsnivaa_kodeliste)
+    
+    return None
 
 if __name__ == "__main__":
     # Example usage of both local and remote codelist checking
