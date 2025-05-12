0a2f89e91f72bfc7c3efe13d85f167421a0de08a
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2025-05-12 09:46:03 +0200

    Oppdater versjon

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.1.0",
+    version="0.1.1",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
