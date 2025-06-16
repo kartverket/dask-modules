1fcab77d8428148c95c81a9365d2d9e8ecba6e25
Sondre Foslien <sondre.foslien@kartverket.no>
2025-06-16 08:35:52 +0200

    Skyporten readme og opprydding (#164)

--- terraform/modules/skyporten_workload_identity/skyporten_consumers/variables.tf
+++ terraform/modules/skyporten_workload_identity/skyporten_consumers/variables.tf
@@ -1,36 +1,35 @@
-variable "org_number" {
-  description = "The organization number that should be allowed to access the Skyporten integration"
-}
-
 variable "region" {
   description = "The region to deploy the resources in"
+  type        = string
 }
 
 variable "workload_identity_pool_id" {
   description = "The ID of the workload pool to be used for the Skyporten integration"
+  type        = string
 }
 
 variable "maskinporten_client_id" {
   description = "The Maskinporten client ID for the consumer"
+  type        = string
 }
 
 variable "project_number" {
   description = "The project number of the project"
+  type        = string
 }
 
 variable "project_id" {
   description = "The project ID of the project"
-}
-
-variable "consumer_name" {
-  description = "The name of the consumer (organization)"
+  type        = string
 }
 
 variable "main_scope" {
   description = "The main organisation scope for the Maskinporten client."
+  type        = string
   default     = "kartverk"
 }
 
 variable "sub_scope" {
   description = "The sub scope for the Maskinporten client."
+  type        = string
 }
