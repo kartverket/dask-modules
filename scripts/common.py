f1b5d5177bf65b267e27f879df9f5f00ad71537d
Jørgen Granseth <jorgen.granseth@kartverket.no>
2026-01-15 15:58:45 +0100

    Fjern dask_infrastructure (flyttet til repoet)

--- scripts/common.py
+++ scripts/common.py
@@ -11,26 +11,10 @@ def replace_special_characters(s):
         'Å': 'Aa'
     }
 
-    def replace(match):
-        return replacements[match.group(0)]
-
-    return re.sub(r'æ|Æ|ø|Ø|å|Å', replace, s)
+    return re.sub(r'[æåÆØÅ]', lambda match: replacements[match.group(0)], s)
 
 def is_content_in_file(file_path: str, content: str) -> bool:
     with open(file_path) as file:
         file_content = file.read()
         return file_content.find(content) > -1
 
-
-def append_content_to_end_of_file(file_path: str, content: str) -> None:
-    lines: List[str] = []
-    with open(file_path) as file:
-        lines = file.readlines()
-
-    if lines and not lines[-1].endswith('\n'):
-        lines[-1] += '\n'
-
-    lines.append(content + '\n')
-
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
