fcf5fa3e068a11cb9c6691c2b2d6a44abfd62a33
Jørgen Granseth <jorgen.granseth@kartverket.no>
2026-01-22 10:38:17 +0100

    Refaktorer data-ingestor-oppsett (#184)
    Refaktorer ingestor-oppsett til å bli mer i tråd med det vi har gjort
    for infrastruktur-repoene.
    - Repo dispatch i stedet for workflow dispatch
    - Flyten trenger ikke være generell lenger nå som dette er eneste PR
    dask-modules har ansvar for å opprette
    - Dropp pubsub-melding i denne omgang. Hvis vi trenger det kan det
    heller legges til, men onboardingsprosessen er potensielt såpass mye
    enklere nå at det ikke vil være behov for det.

--- scripts/data_ingestor_update.py
+++ scripts/data_ingestor_update.py
@@ -1,5 +1,5 @@
+import argparse
 import json
-import sys
 import re
 
 
@@ -16,6 +16,7 @@ def replace_special_characters(s):
     return re.sub(r'[æåÆØÅ]', lambda match: replacements[match.group(0)], s)
 
 
+FILE_PATH = "../"
 envs = ["dev", "prod"]
 
 
@@ -28,9 +29,14 @@ def should_keep_line(line: str) -> bool:
     return False
 
 
-def update_tfvar_file(monorepo_folder_path: str, env: str, project_name: str, project_id: str, project_number: str,
-                      area_name: str) -> None:
-    tfvars_path = f'{monorepo_folder_path}/terraform/variables/{env}.tfvars'
+def update_tfvar_file(
+        env: str,
+        project_name: str,
+        project_id: str,
+        project_number: str,
+        area_name: str,
+) -> None:
+    tfvars_path = f'{FILE_PATH}/terraform/variables/{env}.tfvars'
 
     with open(tfvars_path) as file:
         lines = list(filter(should_keep_line, file.readlines()))
@@ -51,14 +57,14 @@ def update_tfvar_file(monorepo_folder_path: str, env: str, project_name: str, pr
             file.close()
 
 
-def update_state_bucket(monorepo_folder_path: str, env: str, val_for_env: str) -> None:
-    with open(f'{monorepo_folder_path}/terraform/backend/{env}.gcs.tfbackend', 'w') as file:
+def update_state_bucket(env: str, val_for_env: str) -> None:
+    with open(f'{FILE_PATH}/terraform/backend/{env}.gcs.tfbackend', 'w') as file:
         file.write(f'bucket = "{val_for_env}"\n')
         file.close()
 
 
-def update_databricks_bundle_yml(file_path: str, area_name: str, project_name: str):
-    config_path = f'{file_path}/databricks.yml'
+def update_databricks_bundle_yml(area_name: str, project_name: str):
+    config_path = f'{FILE_PATH}/databricks.yml'
 
     with open(config_path, 'r') as file:
         lines = [line.replace("plattform_dataprodukter", f"{area_name.lower()}_{project_name.lower()}") for line in
@@ -70,13 +76,13 @@ def update_databricks_bundle_yml(file_path: str, area_name: str, project_name: s
             file.close()
 
 
-def clear_codeowners(file_path: str, team_name: str):
-    codeowners_path = f'{file_path}/CODEOWNERS'
+def update_codeowners(team_name: str, github_team_name: str):
+    codeowners_path = f'{FILE_PATH}/CODEOWNERS'
 
     with open(codeowners_path, 'r') as file:
         file_content = file.read()
         file_content = file_content.replace("Team DASK (Dataplattform Statens Kartverk)", f"Team {team_name}")
-        file_content = file_content.replace("@kartverket/dask", f"@kartverket/{team_name}")
+        file_content = file_content.replace("@kartverket/dask", f"@kartverket/{github_team_name}")
         file_content = file_content.replace("@sondrfos", "<<enter security champion (@username) here>>")
         file.close()
 
@@ -85,14 +91,14 @@ def clear_codeowners(file_path: str, team_name: str):
             file.close()
 
 
-def clear_catalog_info(file_path: str, team_name: str, project_name: str):
-    cataloginfo_path = f'{file_path}/catalog-info.yaml'
+def update_catalog_info(team_short_name: str):
+    cataloginfo_path = f'{FILE_PATH}/catalog-info.yaml'
 
     with open(cataloginfo_path, 'r') as file:
         file_content = file.read()
-        file_content = file_content.replace("dask-monorepo-reference-setup", f'{project_name.lower()}-data-ingestor')
+        file_content = file_content.replace("dask-monorepo-reference-setup", f'{team_short_name}-data-ingestor')
         file_content = file_content.replace("documentation", "dask jobs")
-        file_content = file_content.replace("dataplattform", team_name)
+        file_content = file_content.replace("dataplattform", team_short_name)
         file.close()
 
         with open(cataloginfo_path, 'w') as file_out:
@@ -100,8 +106,8 @@ def clear_catalog_info(file_path: str, team_name: str, project_name: str):
             file_out.close()
 
 
-def configure_github_deploy_workflow(file_path: str, env: str, project_name: str, project_id: str, project_number: str):
-    workflows_path = f'{file_path}/.github/workflows'
+def configure_github_deploy_workflow(env: str, project_name: str, project_id: str, project_number: str):
+    workflows_path = f'{FILE_PATH}/.github/workflows'
 
     deploy_sa_to_replace = {
         "dev": "dataplattform-deploy@dataprodukter-dev-5daa.iam.gserviceaccount.com",
@@ -138,34 +144,55 @@ def configure_github_deploy_workflow(file_path: str, env: str, project_name: str
             file.close()
 
 
-def edit_file(file_path, json_obj):
-    team_name: str = json_obj.get("team_name")
+def edit_file(json_obj: dict):
+    team_short_name: str = json_obj.get("project_name").lower()
+    team_name: str = json_obj.get("name")
+    github_team_name: str = json_obj.get("git_team_name")
     area_name: str = replace_special_characters(json_obj.get("area_name"))
-    project_name: str = json_obj.get("project_name")
 
-    clear_codeowners(file_path, team_name)
-    update_databricks_bundle_yml(file_path, area_name, project_name)
-    clear_catalog_info(file_path, team_name, project_name)
+    update_codeowners(team_name, github_team_name)
+    update_databricks_bundle_yml(area_name, team_short_name)
+    update_catalog_info(team_short_name)
 
     for env in envs:
         state_bucket_for_env = json_obj.get("gcp_state_buckets")[env]
-        update_state_bucket(file_path, env, state_bucket_for_env)
+        update_state_bucket(env, state_bucket_for_env)
 
         project_id_for_env = json_obj.get("gcp_project_ids")[env]
         auth_project_number_for_env = json_obj.get("gcp_auth_numbers")[env]
-        area_name = json_obj.get("area_name")
-        update_tfvar_file(file_path, env, project_name, project_id_for_env, auth_project_number_for_env, area_name)
+        update_tfvar_file(env, team_short_name, project_id_for_env, auth_project_number_for_env, area_name)
 
-        configure_github_deploy_workflow(file_path, env, project_name, project_id_for_env, auth_project_number_for_env)
+        configure_github_deploy_workflow(env, team_short_name, project_id_for_env, auth_project_number_for_env)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <file_path> <json_object>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Sett opp data-ingestor-repo for et nytt produktteam')
+    payload_format = """
+    {
+        "name": string,
+        "project_name": string,
+        "area_name": string,
+        "git_team_name": string,
+        "gcp_project_ids": {
+            "sandbox": string,
+            "dev": string,
+            "prod": string
+        },
+        "gcp_auth_numbers": {
+            "sandbox": string,
+            "dev": string,
+            "prod": string
+        },
+        "gcp_state_buckets": {
+            "sandbox": string,
+            "dev": string,
+            "prod": string
+        }
+    }
+    """
+    parser.add_argument('--payload', required=True, help=payload_format)
 
-    file_path = sys.argv[1]
-    json_str = sys.argv[2]
-    json_obj = json.loads(json_str)
+    args = parser.parse_args()
+    params = json.loads(args.payload)
 
-    edit_file(file_path, json_obj)
+    edit_file(params)
