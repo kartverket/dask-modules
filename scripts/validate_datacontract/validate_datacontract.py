ed3f2645a601998a58cd8ca0523de0412c05a75d
Sondre Foslien <sondre.foslien@kartverket.no>
2026-02-13 09:32:42 +0100

    Legg til Github Action for å validere og publisere datakontrakt (#200)
    Legger inn github action som validerer alle datakontrakter som har
    endret seg ift main, og pusher disse til et pubsub-topic.
    Validering skjer igjennom et python script. Scriptet med tilhørende
    avhengigheter er definert i `scripts/validate_datacontract/`

---
+++ scripts/validate_datacontract/validate_datacontract.py
@@ -0,0 +1,51 @@
+import os
+from databricks.sdk import WorkspaceClient
+from datacontract.data_contract import DataContract
+from pathlib import Path
+
+if os.environ["ENVIRONMENT"] == "dev":
+    os.environ["DATACONTRACT_DATABRICKS_SERVER_HOSTNAME"] = (
+        "https://2004011444667850.0.gcp.databricks.com"
+    )
+    os.environ["DATACONTRACT_DATABRICKS_HTTP_PATH"] = (
+        "/sql/1.0/warehouses/43ebd38bb3fbe7ac"
+    )
+elif os.environ["ENVIRONMENT"] == "prod":
+    os.environ["DATACONTRACT_DATABRICKS_SERVER_HOSTNAME"] = (
+        "https://3359340685875444.4.gcp.databricks.com"
+    )
+    os.environ["DATACONTRACT_DATABRICKS_HTTP_PATH"] = (
+        "/sql/1.0/warehouses/7b7b34fd5e55d6f1"
+    )
+else:
+    raise ValueError("Invalid environment. Must be 'dev' or 'prod'.")
+
+
+ws_client = WorkspaceClient(
+    host=os.environ["DATACONTRACT_DATABRICKS_SERVER_HOSTNAME"],
+    google_service_account=os.environ["SERVICE_ACCOUNT"],
+)
+
+os.environ["DATACONTRACT_DATABRICKS_TOKEN"] = str(
+    ws_client.api_client._cfg.oauth_token()
+)
+
+changed_datacontracts = os.environ["CHANGED_DATACONTRACTS"]
+changed_datacontracts_list_cleaned = [
+    Path(file.strip())
+    for file in (changed_datacontracts.split(",") if changed_datacontracts else [])
+    if file.strip()
+]
+
+has_failed = False
+for file in changed_datacontracts_list_cleaned:
+    if file.suffix in [".yml", ".yaml"]:
+        data_contract = DataContract(data_contract_file=str(file))
+        run = data_contract.test()
+        if not run.has_passed():
+            has_failed = True
+            print(f"Data contract {file} failed tests:")
+            print(run.pretty_logs())
+
+if has_failed:
+    raise Exception("Some data contracts failed tests. See logs for details.")
